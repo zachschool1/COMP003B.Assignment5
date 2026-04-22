df53be2fc706fe8104e70ff50adcb235c367d692
zachschool1 <zacharyjordan@my.whccd.edu>
2026-04-22 10:31:03 -0700

    Add project files.

---
+++ Data/AnimalStore.cs
@@ -0,0 +1,13 @@
+﻿using COMP003B.Assignment5.Models;
+namespace COMP003B.Assignment5.Data;
+
+public class AnimalStore
+{
+    public static List<Animal> Animal { get; } = new()
+    {
+        new Animal { Id =  1, Name = "Fluffy", Breed = "Pomeranian", Weight = 9},
+        new Animal { Id =  2, Name = "Jackson", Breed = "Terrier", Weight = 14.3},
+        new Animal { Id =  3, Name = "Beau", Breed = "German Shepherd", Weight = 59},
+        new Animal { Id =  4, Name = "Jimmy", Breed = "Rottweiler", Weight = 39},
+    };
+}
---
+++ Models/Animal.cs
@@ -0,0 +1,10 @@
+﻿namespace COMP003B.Assignment5.Models
+{
+    public class Animal
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Breed { get; set; }
+        public double Weight { get; set; }
+    }
+}
